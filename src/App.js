c1b126c29270e9edf4b30bdc4fbea9c159d44058
rlevec <rene.levec@outlook.com>
2022-06-19 20:05:03 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
 
-function App() {
+const url = "http://localhost:5000/users"
+
+const App = () => {
+  const [loading, setLoading] = useState(true)
+  const [paragraphs, setParagraphs] = useState([])
+  const [text, setText] = useState([])
+  const [count, setCount] = useState(0)
+
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let amount = parseInt(count)
+    if(count <= 0) amount = 1
+    if(count > paragraphs.length) amount = paragraphs.length
+    setText(paragraphs.slice(0, amount))
+  }
+
+
+  const fetchUrl = async() => {
+    const response = await fetch(url)
+    const newParagraphs = await response.json()
+    setParagraphs(newParagraphs)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchUrl()
+  }, [])
+
+  if(loading) {
+    return (
+      <section className='section-center'>
+        <h2>loading...</h2>
+      </section>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className='section-center'>
+      <h3>lorem ipsum paragraph generator</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraphs: </label>
+        <input 
+          type='number'
+          id='amount'
+          name='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className='btn' type='submit'>generate</button>
+      </form>
+      {
+        text.map((paragraph, index) => {
+          return (
+            <div key={index}>
+              <p>{paragraph}</p>
+              <div className='underline'></div>
+            </div>
+          )
+        })
+      }
+    </section>
+  )
 }
 
-export default App;
+export default App
